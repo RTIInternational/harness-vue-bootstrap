490bddcc8e9b8aea42bdcf93e35fcd3302e4e028
Alex Harding <alexharding@rti.org>
2024-09-13 13:28:30 -0400

    remove loadData functionality from boundValue this goes hand in hand with harness-vue's change to handle synchronous behavior

--- src/components/inputs/utils/useBoundValue.js
+++ src/components/inputs/utils/useBoundValue.js
@@ -2,8 +2,7 @@ import { computed } from "vue";
 
 /**
  * This function returns a computed variable for use with v-model, binding harness-vue store attributes to inputs.
- * We use this instead of binding directly to the store so that we can intercept and enforce the "synchronous" behavior in the "set"
- * This also allows us to use getFilter/setFilter, which we have listeners on for beforeSet/afterSet behavior
+ * We use this instead of binding directly to the store so that we are dispatching actions which may have subscriptions
  * @param {Object} props Component Props
  * @param {Object} harness the harness instance
  * @returns {any} a vue computed
@@ -14,14 +13,11 @@ export default function useBoundValue(props, harness) {
       try {
         return harness.getFilter(props.filter.key);
       } catch {
-        return "asd";
+        return null;
       }
     },
     set(value) {
       harness.setFilter(props.filter.key, value);
-      if (!props.synchronous && harness.pageDefinition.loadData) {
-        harness.loadData();
-      }
     },
   });
 }
