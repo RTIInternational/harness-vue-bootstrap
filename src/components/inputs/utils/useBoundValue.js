06d37280349b9abf448506c9d090964e1c961aaa
Alex Harding <alexharding@rti.org>
2024-09-16 14:14:47 -0400

    restoring synchronous behavior with new syntax

--- src/components/inputs/utils/useBoundValue.js
+++ src/components/inputs/utils/useBoundValue.js
@@ -2,7 +2,8 @@ import { computed } from "vue";
 
 /**
  * This function returns a computed variable for use with v-model, binding harness-vue store attributes to inputs.
- * We use this instead of binding directly to the store so that we are dispatching actions which may have subscriptions
+ * We use this instead of binding directly to the store so that we can intercept and enforce the "synchronous" behavior in the "set"
+ * This also allows us to use getFilter/setFilter, which we have listeners on for beforeSet/afterSet behavior
  * @param {Object} props Component Props
  * @param {Object} harness the harness instance
  * @returns {any} a vue computed
@@ -10,14 +11,10 @@ import { computed } from "vue";
 export default function useBoundValue(props, harness) {
   return computed({
     get() {
-      try {
-        return harness.getFilter(props.filter.key);
-      } catch {
-        return null;
-      }
+      return harness.getFilter(props.filter.key);
     },
     set(value) {
-      harness.setFilter(props.filter.key, value);
+      harness.setFilter(props.filter.key, value, !props.synchronous);
     },
   });
 }
